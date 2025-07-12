e1a5d8854d8add16f779e9f4752bdf580dc271dc
오지환 <ovitwisdom@gmail.com>
2025-07-12 10:31:42 +0900

    배포2

--- backend/app.py
+++ backend/app.py
@@ -16,6 +16,7 @@ CORS(app)
 # ✅ 절대경로 기반 SQLite 설정
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DB_PATH = os.path.join(BASE_DIR, 'instance', 'db.sqlite3')
+os.makedirs(os.path.dirname(DB_PATH), exist_ok=True)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_PATH}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
